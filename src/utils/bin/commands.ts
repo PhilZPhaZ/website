cb3058a9e15fcae3e245fe0933d23ef9a98096ce
Phileas Bour <bour.phileas@gmail.com>
2025-01-16 19:18:23 +0100

    update .next and help command

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -8,11 +8,7 @@ export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
   var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
-    if (i % 7 === 0) {
       c += Object.keys(bin).sort()[i - 1] + '\n';
-    } else {
-      c += Object.keys(bin).sort()[i - 1] + ' ';
-    }
   }
   return `Bienvenue! Ici s'affiche la liste des commandes disponibles:
 \n${c}\n
